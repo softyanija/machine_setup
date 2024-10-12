42de05dc7ad57fbd20c6b3509a4015ebdd2570c2
softyanija <amakou0614@gmail.com>
2024-10-12 20:14:15 +0900

    add: Added sudo

--- machine_setup.bash
+++ machine_setup.bash
@@ -3,6 +3,6 @@
 sudo apt install emacs git tmux tree
 
 #run sub scripts 
-./emacs/setup_emacs.sh
-./gitconfig/setup_gitconfig.sh
-./tmux/setup_tmux.sh
+sudo ./emacs/setup_emacs.sh
+sudo ./gitconfig/setup_gitconfig.sh
+sudo ./tmux/setup_tmux.sh
