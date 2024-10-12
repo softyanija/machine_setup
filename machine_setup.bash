fb5ec53feb91e49d3015084c8ab1a04e304af271
softyanija <amakou0614@gmail.com>
2024-10-12 21:04:17 +0900

    fix: Added bash forgot to wrote

--- machine_setup.bash
+++ machine_setup.bash
@@ -3,6 +3,6 @@
 sudo apt install emacs git tmux tree
 
 #run sub scripts 
-sudo ./emacs/setup_emacs.sh
-sudo ./gitconfig/setup_gitconfig.sh
-sudo ./tmux/setup_tmux.sh
+sudo bash ./emacs/setup_emacs.sh
+sudo bash ./gitconfig/setup_gitconfig.sh
+sudo bash ./tmux/setup_tmux.sh
