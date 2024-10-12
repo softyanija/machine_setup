13094e5aae000eed9b0f424042c4f255dad712f7
softyanija <amakou0614@gmail.com>
2024-10-12 21:02:11 +0900

    fix: Fixed wrong sysmbolic link in emacs

--- emacs/setup_emacs.sh
+++ emacs/setup_emacs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ln -s ./init.el  $HOME/.emacs.d/init.el
+USER_HOME=$(eval echo ~$SUDO_USER)
+PROJECT_DIR="${USER_HOME}/machine_setup"
+
+ln -s $PROJECT_DIR/emacs/init.el $USER_HOME/.emacs.d/init.el
