b526320e0258ef2a2ef1cf7e5346252a12ebee6b
softyanija <amakou0614@gmail.com>
2025-03-14 19:35:38 +0900

    fix: fixed to create ~/.emacs.d if not exist

--- emacs/setup_emacs.sh
+++ emacs/setup_emacs.sh
@@ -2,5 +2,10 @@
 
 USER_HOME=$(eval echo ~$SUDO_USER)
 PROJECT_DIR="${USER_HOME}/machine_setup"
+EMACS_DIR="${USER_HOME}/.emacs.d"
+
+if [ ! -d "$EMACS_DIR" ]; then
+  mkdir -p "$EMACS_DIR"
+fi
 
 ln -s $PROJECT_DIR/emacs/init.el $USER_HOME/.emacs.d/init.el
